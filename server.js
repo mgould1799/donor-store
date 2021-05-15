1edcf7d8ea17b093dae1d7dd353d7c189e8b04f8
Meagan <gouldma@g.cofc.edu>
2021-05-15 12:27:53 -0400

    adds mysql db connection to server.js

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
+// https://www.codementor.io/@olatundegaruba/nodejs-restful-apis-in-10-minutes-q0sgsfhbd
+
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 8080,
+  mysql = require('mysql'),
   bodyParser = require('body-parser');
 
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
 
-  var routes = require('./api/routes/donor-routes'); //importing route
-  routes(app);
+var con = mysql.createConnection({
+    host: "db",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+var routes = require('./api/routes/donor-routes'); //importing route
+routes(app);
 
 app.listen(port);
 
