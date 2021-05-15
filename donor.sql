81d10c6cd03380a33b608587a93b023681d56bde
Meagan <gouldma@g.cofc.edu>
2021-05-15 12:16:50 -0400

    adds sql schema

---
+++ donor.sql
@@ -0,0 +1,42 @@
+#
+# TABLE STRUCTURE FOR: comments
+#
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `donor_id` int(9) unsigned NOT NULL,
+  `comment` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# TABLE STRUCTURE FOR: donation
+#
+
+CREATE TABLE `donation` (
+  `id` int(11) NOT NULL,
+  `donor_id` int(9) unsigned NOT NULL,
+  `frequency_of_donation` varchar(255) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `currency` varchar(255) NOT NULL,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# TABLE STRUCTURE FOR: donor
+#
+
+CREATE TABLE `donor` (
+  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+  `last_name` varchar(100) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `street_address` varchar(255) NOT NULL,
+  `city` char(10) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `poastal_code` varchar(255) NOT NULL,
+  `phone_number` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `preferred_payment` varchar(255) NOT NULL,
+  `preferred_form_contact` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 8080,
+  bodyParser = require('body-parser');
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+
+  var routes = require('./api/routes/donor-routes'); //importing route
+  routes(app);
+
+app.listen(port);
+
+console.log('donor-store running on: ' + port);
